2f945bf8b85fa514047086617f2fb10c9d3852ee
Kirill <leonov835@yandex.ru>
2023-07-23 22:29:20 +0300

    init

--- lib/bacon_ipsum.rb
+++ lib/bacon_ipsum.rb
@@ -4,8 +4,13 @@ require_relative 'bacon_ipsum/http_client'
 require_relative 'bacon_ipsum/parser'
 
 module BaconIpsum
-  def self.generate(paras: 5, sentences: nil)
-    response = HttpClient.new(paras, sentences)
-    Parser.parse(response.get)
+  def self.generate(paras: 5,
+                    sentences: nil,
+                    type: 'meat-and-filler',
+                    start_with_lorem: nil,
+                    format: 'json')
+
+    response = HttpClient.new(paras, sentences, type, start_with_lorem, format)
+    Parser.new(response.get, format).parse
   end
 end
--- spec/lib/bacon_ipsum/http_client_spec.rb
+++ spec/lib/bacon_ipsum/http_client_spec.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe BaconIpsum::HttpClient do
-  subject(:http_client) { described_class.new(paras, sentences) }
+  subject(:http_client) { described_class.new(paras, sentences, type, start_with_lorem, format) }
 
   let(:paras) { 3 }
   let(:sentences) { 5 }
+  let(:type) { 'all-meat' }
+  let(:start_with_lorem) { 1 }
+  let(:format) { 'json' }
 
   describe '#initialize' do
     it 'sets the correct values for paras and sentences' do
       expect(http_client.paras).to eq(paras)
       expect(http_client.sentences).to eq(sentences)
+      expect(http_client.type).to eq(type)
+      expect(http_client.start_with_lorem).to eq(start_with_lorem)
+      expect(http_client.format).to eq(format)
     end
   end
 
