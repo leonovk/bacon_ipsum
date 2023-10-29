538ef8d78a31cb6a279c513fefd5c6325de255ec
Kirill Leonov <leonov835@yandex.ru>
2023-10-29 23:21:30 +0300

    remove extra gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,7 +4,6 @@ require_relative '../lib/bacon_ipsum/http_client'
 require_relative '../lib/bacon_ipsum/parser'
 require_relative '../lib/bacon_ipsum/version'
 require_relative '../lib/bacon_ipsum'
-require 'webmock/rspec'
 require 'json'
 require 'byebug'
 
